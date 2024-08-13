7ead46766c40648c2652edf5f6af08103b8159e5
Luiscv1719 <luiscv1719@gmail.com>
2024-08-12 23:37:54 -0500

    creacion de archivos de conexion con db

---
+++ API/Models/Dto/ResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models.Dto
+{
+    public class ResponseDto
+    {
+        public object? Data { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+
+    }
+}

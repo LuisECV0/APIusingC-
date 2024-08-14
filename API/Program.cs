60072a46eb983e814e37479f5498857b54bcfda1
Luiscv1719 <luiscv1719@gmail.com>
2024-08-13 23:43:30 -0500

    cambios en conextion

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,9 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext> (options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
